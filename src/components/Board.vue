0bc2c1d192f524f882ec6a1700bfa58902f46f05
KushibikiMashu <kushibiki3413@gmail.com>
2021-01-06 00:28:12 +0900

    feat: Board を追加

---
+++ src/components/Board.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+<div class="status">status</div>
+<div class="board-row">
+  <square />
+  <square />
+  <square />
+</div>
+<div class="board-row">
+  <square />
+  <square />
+  <square />
+</div>
+<div class="board-row">
+  <square />
+  <square />
+  <square />
+</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Square from './Square.vue'
+
+export default defineComponent({
+  name: 'Board',
+  components: {
+    Square
+  },
+  data () {
+    return {
+      status: 'Next player: X'
+    }
+  }
+})
+</script>
+
+<style scoped>
+.status {
+  margin-bottom: 10px;
+}
+
+.board-row:after {
+  clear: both;
+  content: "";
+  display: table;
+}
+</style>
