d118f8377107cd8b149e33f10ea6bd1e7dcf9332
KushibikiMashu <kushibiki3413@gmail.com>
2021-01-06 00:28:21 +0900

    feat: Game を追加

---
+++ src/components/Game.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="game">
+  <div class="game-board">
+    <board />
+  </div>
+  <div class="game-info">
+    <div>status</div>
+    <ol>todo</ol>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Board from './Board.vue'
+
+export default defineComponent({
+  name: 'Game',
+  components: { Board }
+})
+</script>
+
+<style scoped>
+.game {
+  display: flex;
+  flex-direction: row;
+}
+
+.game-board {
+
+}
+
+.game-info {
+  margin-left: 20px;
+}
+
+ol, ul {
+  padding-left: 30px;
+}
+</style>
