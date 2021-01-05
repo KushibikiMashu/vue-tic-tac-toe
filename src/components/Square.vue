be35eb7a92f98616a8f27e35497072e15115e348
KushibikiMashu <kushibiki3413@gmail.com>
2021-01-06 00:28:01 +0900

    feat: Square を追加

---
+++ src/components/Square.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="square">
+<!--    TODO-->
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Square'
+})
+</script>
+
+<style scoped>
+.square {
+  background: #fff;
+  border: 1px solid #999;
+  float: left;
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 34px;
+  height: 34px;
+  margin-right: -1px;
+  margin-top: -1px;
+  padding: 0;
+  text-align: center;
+  width: 34px;
+}
+
+.square:focus {
+  outline: none;
+  background: #ddd;
+}
+</style>
