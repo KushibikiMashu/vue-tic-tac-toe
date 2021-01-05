a09f87e740455f7e59dc29374d486036fd19b9ca
KushibikiMashu <kushibiki3413@gmail.com>
2021-01-06 00:06:12 +0900

    feat: Layoutを追加

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <layout>
+    <template v-slot:default>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    </template>
+  </layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import Layout from './components/Layout.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Layout
   }
 })
 </script>
@@ -20,8 +26,6 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav>
+    <h1>Tic-Tac-Toe by Vue.js</h1>
+  </nav>
+  <main>
+    <slot></slot>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Layout'
+})
+</script>
+
+<style scoped>
+
+</style>
