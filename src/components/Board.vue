56df0390bfb8bfc7f3a741e8f6ad143d298eb8a0
KushibikiMashu <kushibiki3413@gmail.com>
2021-01-07 21:58:58 +0900

    feat: Board コンポーネントのメソッドを Game コンポーネントで定義する

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
-    <div class="status">{{ status }}</div>
     <div class="board-row">
-      <square :value="squares[0]" :onclick="() => handleClick(0)"/>
-      <square :value="squares[1]" :onclick="() => handleClick(1)"/>
-      <square :value="squares[2]" :onclick="() => handleClick(2)"/>
+      <square :value="squares[0]" :onclick="() => onclick(0)"/>
+      <square :value="squares[1]" :onclick="() => onclick(1)"/>
+      <square :value="squares[2]" :onclick="() => onclick(2)"/>
     </div>
     <div class="board-row">
-      <square :value="squares[3]" :onclick="() => handleClick(3)"/>
-      <square :value="squares[4]" :onclick="() => handleClick(4)"/>
-      <square :value="squares[5]" :onclick="() => handleClick(5)"/>
+      <square :value="squares[3]" :onclick="() => onclick(3)"/>
+      <square :value="squares[4]" :onclick="() => onclick(4)"/>
+      <square :value="squares[5]" :onclick="() => onclick(5)"/>
     </div>
     <div class="board-row">
-      <square :value="squares[6]" :onclick="() => handleClick(6)"/>
-      <square :value="squares[7]" :onclick="() => handleClick(7)"/>
-      <square :value="squares[8]" :onclick="() => handleClick(8)"/>
+      <square :value="squares[6]" :onclick="() => onclick(6)"/>
+      <square :value="squares[7]" :onclick="() => onclick(7)"/>
+      <square :value="squares[8]" :onclick="() => onclick(8)"/>
     </div>
   </div>
 </template>
@@ -28,53 +27,9 @@ export default defineComponent({
   components: {
     Square
   },
-  data () {
-    return {
-      status: 'Next player: X',
-      squares: Array(9).fill(null),
-      xIsNext: true
-    }
-  },
-  methods: {
-    handleClick (i: number) {
-      if (this.calculateWinner(this.squares) || this.squares[i]) {
-        return
-      }
-
-      this.squares[i] = this.xIsNext ? 'X' : 'O'
-      this.xIsNext = !this.xIsNext
-      this.updateStatus()
-    },
-    calculateWinner (squares: ('X' | 'O' | null)[]) {
-      const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-      ]
-      for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i]
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-          return squares[a]
-        }
-      }
-      return null
-    },
-    updateStatus () {
-      const winner = this.calculateWinner(this.squares)
-      let status
-      if (winner) {
-        status = 'Winner: ' + winner
-      } else {
-        status = `Next player: ${this.xIsNext ? 'X' : 'O'}`
-      }
-
-      this.status = status
-    }
+  props: {
+    squares: Array,
+    onclick: Function
   }
 })
 </script>
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,22 +1,81 @@
 <template>
 <div class="game">
   <div class="game-board">
-    <board />
+    <board :squares="getCurrent" :onclick="(i) => handleClick(i)" />
   </div>
   <div class="game-info">
-<!--    <div>status</div>-->
+    <div>{{ getStatus }}</div>
 <!--    <ol>todo</ol>-->
   </div>
 </div>
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent } from 'vue'
 import Board from './Board.vue'
 
 export default defineComponent({
   name: 'Game',
-  components: { Board }
+  components: { Board },
+  data () {
+    return {
+      status: 'Next player: X',
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      xIsNext: true
+    }
+  },
+  computed: {
+    getStatus () {
+      const current = this.history[this.history.length - 1]
+      const winner = this.calculateWinner(current.squares)
+      let status
+      if (winner) {
+        status = 'Winner: ' + winner
+      } else {
+        status = `Next player: ${this.xIsNext ? 'X' : 'O'}`
+      }
+
+      return status
+    },
+    getCurrent () {
+      return this.history[this.history.length - 1].squares
+    }
+  },
+  methods: {
+    handleClick (i) {
+      const current = this.history[this.history.length - 1]
+      const squares = current.squares.slice()
+      if (this.calculateWinner(squares) || squares[i]) {
+        return
+      }
+
+      squares[i] = this.xIsNext ? 'X' : 'O'
+
+      this.history = this.history.concat([{ squares }])
+      this.xIsNext = !this.xIsNext
+    },
+    calculateWinner (squares) {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ]
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i]
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+          return squares[a]
+        }
+      }
+      return null
+    }
+  }
 })
 </script>
 
