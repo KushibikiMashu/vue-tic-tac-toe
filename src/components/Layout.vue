cda668892278cbf48569505140933d1277ba4d8e
KushibikiMashu <kushibiki3413@gmail.com>
2021-01-10 01:13:42 +0900

    feat: GitHub, Twitter へのリンクを追加

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,6 +1,11 @@
 <template>
   <nav>
     <h1>Tic-Tac-Toe by Vue.js</h1>
+    <p>
+      <a href="https://github.com/KushibikiMashu/vue-tic-tac-toe">GitHub</a>
+      {{ ' ' }}
+      <a href="https://twitter.com/Panda_Program">Twitter</a>
+    </p>
   </nav>
   <main>
     <slot></slot>
