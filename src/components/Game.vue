c7346ae9eeffcea70680a3d04031dcae87a4f3e9
KushibikiMashu <kushibiki3413@gmail.com>
2021-01-14 20:46:59 +0900

    refactor: Composition API にリファクタ

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -2,7 +2,7 @@
   <div class="game">
     <div>
       <board
-        :squares="current"
+        :squares="currentSquares"
         :winnerCells="winnerCells"
         v-on:clickSquare="handleClick"
       />
@@ -13,13 +13,13 @@
         <button @click="toggle">{{ isDesc ? '降順' : '昇順' }}</button>
       </div>
       <ol>
-        <li v-for="({ location }, move) in history" :key="move">
-          <button @click="jumpTo(move)">
-            <span :class="{'game-stepButton-bold': move === stepNumber}">
-              {{ isDesc ? getDescButtonBody(move) : getAscButtonBody(move) }}
+        <li v-for="(step, i) in history" :key="i">
+          <button @click="jumpTo(i)">
+            <span :class="{'game-stepButton-bold': i === stepNumber}">
+              {{ isDesc ? getDescButtonBody(i) : getAscButtonBody(i) }}
             </span>
           </button>
-          <span v-if="location.row && location.column">（{{ location.row }}, {{ location.column }}）</span>
+<!--          <span v-if="step.location.row && step.location.column">（{{ step.location.row }}, {{ step.location.column }}）</span>-->
         </li>
       </ol>
     </div>
@@ -27,65 +27,30 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, reactive, ref, computed } from 'vue'
 import Board from './Board.vue'
 
 export default defineComponent({
   name: 'Game',
   components: { Board },
-  data () {
-    return {
-      history: [{
-        squares: Array(9).fill(null),
-        location: { row: null, column: null }
-      }],
-      stepNumber: 0,
-      xIsNext: true,
-      isDesc: true,
-      winnerCells: null
-    }
-  },
-  computed: {
-    status () {
-      const current = this.history[this.history.length - 1]
-      const winner = this.calculateWinner(current.squares)
-      let status
-
-      if (winner) {
-        status = 'Winner: ' + winner
-      } else if (this.history.length === 10 && this.winnerCells === null) {
-        status = 'No winner'
-      } else {
-        status = `Next player: ${this.xIsNext ? 'X' : 'O'}`
+  setup () {
+    const history = reactive([{
+      squares: Array(9).fill(null),
+      location: {
+        row: null,
+        column: null
       }
+    }])
+    const stepNumber = ref(0)
+    const xIsNext = ref(true)
+    const isDesc = ref(true)
+    const winnerCells = ref(null)
 
-      return status
-    },
-    current () {
-      return this.history[this.stepNumber].squares
-    }
-  },
-  methods: {
-    handleClick (i) {
-      if (this.winnerCells) {
-        return
-      }
+    const currentSquares = computed(() => {
+      return history[stepNumber.value].squares
+    })
 
-      const current = this.history[this.history.length - 1]
-      const squares = current.squares.slice()
-      if (this.calculateWinner(squares) || squares[i]) {
-        return
-      }
-
-      squares[i] = this.xIsNext ? 'X' : 'O'
-      const location = { row: Math.floor(i / 3) + 1, column: i % 3 + 1 }
-
-      const newHistory = [{ squares, location }]
-      this.history = this.isDesc ? this.history.concat(newHistory) : newHistory.concat(this.history)
-      this.stepNumber += 1
-      this.xIsNext = !this.xIsNext
-    },
-    calculateWinner (squares) {
+    const calculateWinner = (squares) => {
       const lines = [
         [0, 1, 2],
         [3, 4, 5],
@@ -99,29 +64,84 @@ export default defineComponent({
       for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i]
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-          this.winnerCells = lines[i]
+          winnerCells.value = lines[i]
           return squares[a]
         }
       }
 
       return null
-    },
-    jumpTo (step) {
-      this.stepNumber = step
-      this.xIsNext = (step % 2) === 0
-    },
-    toggle () {
-      this.isDesc = !this.isDesc
-      this.stepNumber = this.history.length - 1 - this.stepNumber
-      this.history = this.history.reverse()
-    },
-    getDescButtonBody (move) {
-      return move ? `Go to move #${move}` : 'Go to game start'
-    },
-    getAscButtonBody (move) {
-      const length = this.history.length - 1
+    }
 
-      return move === length ? 'Go to game start' : `Go to move #${length - move}`
+    const current = history[history.length - 1]
+
+    const handleClick = (i) => {
+      // 勝者が決まったらマークは追加できない
+      if (winnerCells.value) {
+        return
+      }
+
+      const squares = current.squares
+
+      if (calculateWinner(squares) || squares[i]) {
+        return
+      }
+
+      squares[i] = xIsNext.value ? 'X' : 'O'
+      const location = {
+        row: Math.floor(i / 3) + 1,
+        column: i % 3 + 1
+      }
+      const newHistory = { squares, location }
+      isDesc.value ? history.push(newHistory) : history.unshift(newHistory)
+      stepNumber.value += 1
+      xIsNext.value = !xIsNext.value
+    }
+
+    // status
+    const winner = computed(() => calculateWinner(current.squares))
+    const status = computed(() => {
+      if (winner.value) {
+        return 'Winner: ' + winner.value
+      } else if (history.length === 10 && winnerCells.value === null) {
+        return 'No winner'
+      }
+
+      return `Next player: ${xIsNext.value ? 'X' : 'O'}`
+    })
+
+    const jumpTo = (step) => {
+      stepNumber.value = step
+      xIsNext.value = (step % 2) === 0
+    }
+
+    const toggle = () => {
+      isDesc.value = !isDesc.value
+      stepNumber.value = history.length - 1 - stepNumber.value
+      history.value = history.reverse()
+    }
+
+    const getAscButtonBody = (i) => {
+      const length = history.length - 1
+
+      return i === length ? 'Go to game start' : `Go to move #${length - i}`
+    }
+
+    const getDescButtonBody = (i) => {
+      return i ? `Go to move #${i}` : 'Go to game start'
+    }
+
+    return {
+      history,
+      currentSquares,
+      stepNumber,
+      status,
+      isDesc,
+      winnerCells,
+      handleClick,
+      jumpTo,
+      toggle,
+      getAscButtonBody,
+      getDescButtonBody
     }
   }
 })
